ef8e5c9d7e51fe63dc4d6e5b5655e08b21425b26
Andrew Williams <andy@tensixtyone.com>
2024-04-01 11:27:41 +0100

    Add meta

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs ? import <nixpkgs> { system = builtins.currentSystem; },
   poetry2nix ? import (fetchTarball {
     url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.2.2230616.tar.gz";
     sha256 = "0qx2iv57vhgraaqj4dm9zd3dha1p6ch4n07pja0hsxsymjbvdanw";
@@ -7,4 +8,16 @@
 
 poetry2nix.mkPoetryApplication {
   projectDir = ./.;
+  pyproject = ./pyproject.toml;
+  poetrylock = ./poetry.lock;
+
+  pythonImportsCheck = [ "tildejsongen" ];
+
+  meta = with pkgs.lib; {
+      description = "A simple Python script to generate a `tilde.json` file for Tilde-style servers";
+      homepage = "https://github.com/dimension-sh/tildejsongen";
+      maintainers = with maintainers; [ nikdoof ];
+      license = licenses.mit;
+      platforms = platforms.all;
+  };
 }
