f9ba3dada373065d89642f7d52500ebad30f9d7b
Andrew Williams <andy@tensixtyone.com>
2024-04-01 10:50:48 +0100

    Update to working Nix config

--- default.nix
+++ default.nix
@@ -1,4 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.poetry2nix.mkPoetryApplication {
+{
+  poetry2nix ? import (fetchTarball {
+    url = "https://github.com/nix-community/poetry2nix/archive/refs/tags/2024.2.2230616.tar.gz";
+    sha256 = "0qx2iv57vhgraaqj4dm9zd3dha1p6ch4n07pja0hsxsymjbvdanw";
+  }) {}
+}:
+
+poetry2nix.mkPoetryApplication {
   projectDir = ./.;
 }
